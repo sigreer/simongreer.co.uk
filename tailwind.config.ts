50c690a43eea278505ec67d7abe7972f36eb1047
sigreer <simon@sidewayssystems.co.uk>
2024-11-10 12:15:13 +0000

    icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,10 @@ const config: Config = {
 	theme: {
 		extend: {
 			maxWidth: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
 				'2xl': '1536px',
 				'3xl': '1920px',
 			},
