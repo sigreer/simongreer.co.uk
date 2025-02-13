03115ea02fdbc87cb1e2a0a2459ebf6db8ed8ffb
Simon Greer <simon@sidewayssystems.co.uk>
2025-02-13 22:51:51 +0000

    added web-development content

--- src/content/config.ts
+++ src/content/config.ts
@@ -21,7 +21,7 @@ const tech = defineCollection({
     vendor_secondary_color: z.string().optional(),
     tags: z.array(reference('tags')).optional(),
     category: z.enum(['development', 'cloud', 'systems', 'business', 'ai-automation', 'data', 'media']),
-    hireme_filter: z.enum(['broadcast-networking', 'web-development', 'software-development', 'cloud-and-hosted', 'ai-automation', 'networking', 'storage', 'system-administration', 'design-and-deployment', 'business', 'data-and-databases'])
+    hireme_filter: z.enum(['broadcast-networking', 'web-development', 'software-development', 'cloud-and-hosted', 'ai-automation', 'networking', 'storage', 'system-administration', 'design-and-deployment', 'business', 'data-and-databases', 'media-server'])
   })
 });
 
@@ -41,6 +41,7 @@ const projects = defineCollection({
     tags: z.array(reference('tags')).optional(),
     client: reference('clients').optional(),
     vendor: z.string().optional(),
+    website: z.string().optional(),
   }),
 });
 
@@ -55,6 +56,7 @@ const clients = defineCollection({
     industry: z.string().nullable(),
     tags: z.array(reference('tags')).optional(),
     horizontal_logo: z.string().optional().describe('Path to the horizontal logo image'),
+    contact_name: z.string().optional(),
   }),
 });
 
@@ -85,8 +87,27 @@ const tags = defineCollection({
     name: z.string(),
     description: z.string().optional(),
     color: z.string().optional(),
-    synonyms: z.array(z.string()).optional()
+    synonyms: z.array(z.string()).optional(),
+    category: z.string().optional()
   })
 });
 
-export const collections = { blog, projects, tech, tags, clients };
+const testimonials = defineCollection({
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "./src/content/testimonials/"
+  }),
+  schema: z.object({
+    headline: z.string(),
+    description: z.string(),
+    client: reference('clients'),
+    client_contact_name: z.string(),
+    client_name: reference('clients'),
+    project: reference('projects').optional(),
+    project_description: z.string().optional(),
+    tags: z.array(reference('tags')).optional(),
+    status: z.enum(['draft', 'published', 'archived', 'hidden'])
+  })
+});
+
+export const collections = { blog, projects, tech, tags, clients, testimonials };
