bd915353ec429d82f866a234656bad9b869669cd
Simon Greer <me@simongreer.co.uk>
2025-11-07 04:40:04 +0000

    FEATURE: Add pattern plugin and extend Tailwind color palette
    Integrated tailwindcss-bg-patterns plugin and extended theme colors with new shade variations (themepink, themepurple) for more granular design control.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,31 @@ const config: Config = {
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,scss}',
 	],
 	theme: {
+
+
     	extend: {
+					patterns: {
+        opacities: {
+            100: "1",
+            80: ".80",
+            60: ".60",
+            40: ".40",
+            20: ".20",
+            10: ".10",
+            5: ".05",
+        },
+        sizes: {
+            1: "0.25rem",
+            2: "0.5rem",
+            4: "1rem",
+            6: "1.5rem",
+            8: "2rem",
+            16: "4rem",
+            20: "5rem",
+            24: "6rem",
+            32: "8rem",
+        }
+    },
     		maxWidth: {
     			sm: '640px',
     			md: '768px',
@@ -157,7 +181,8 @@ const config: Config = {
 	plugins: [
 		typography,
         require("tailwindcss-animate"),
-        require("tailwindcss-motion")
+        require("tailwindcss-motion"),
+		require('tailwindcss-bg-patterns'),
     ],
 };
 
